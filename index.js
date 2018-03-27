fadc013f3dcb399471bc112f37f3ddeaca75e673
1ung <leewhe@hotmail.com>
2018-03-28 00:54:00 +0800

    completed delv w/ bug

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const exphbs = require('express-handlebars');
-const { Client } = require('pg');
+const {
+  Client
+} = require('pg');
 
 // Initialise postgres client
-const client = new Client({
-  user: 'akira',
-  host: '127.0.0.1',
-  database: 'pokemons',
-  port: 5432,
-});
+// const client = new Client({
+//   user: '1ung',
+//   host: '127.0.0.1',
+//   database: 'pokemons',
+//   port: 5432,
+// });
 
 /**
  * ===================================
@@ -21,7 +23,9 @@ const client = new Client({
 // Init express app
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(methodOverride('_method'));
 
 
@@ -35,11 +39,178 @@ app.set('view engine', 'handlebars');
  * Routes
  * ===================================
  */
+app.get('/:id/edit', (request, response) => {
 
-app.get('/', (req, res) => {
+  let client = new Client({
+    user: '1ung',
+    host: '127.0.0.1',
+    database: 'pokemons',
+    port: 5432
+  });
+
+  let context = {}
+
+  client.connect((err) => {
+    if (err) {
+      console.log("connection error", err.message);
+    }
+
+    let query = "SELECT * FROM pokemon WHERE id = $1;";
+    values = [request.params.id];
+
+    client.query(query, values, (err, res) => {
+      
+      if (err) {
+        console.log("query error", err.message);
+      } else {
+        context.pokemon = res.rows[0];
+      }
+      response.render('edit', context);
+      client.end();
+    });
+  });
+});
+
+app.put('/:id', (reqest, response) => {
+  let client = new Client({
+    user: '1ung',
+    host: '127.0.0.1',
+    database: 'pokemons',
+    port: 5432
+  });
+
+  let pokeData = request.body;
+  let query = "UPDATE pokemon SET num=$1, name=$2, img=$3, weight=$4, height=$5;";
+  let values = [pokeData.num, pokeData.name, pokeData.img, pokeData.weight, pokeData.height];
+
+  client.connect((err) => {
+    if (err) {
+      console.log("connection error", err.message);
+    }
+    client.query(query, values, (err, res) => {
+      if (err) {
+        console.log("query error", err.message);
+      }
+      response.redirect('/' + pokeData.id);
+      client.end();
+    });
+  });
+});
+
+app.delete('/:id', (request, response) => {
+  let client = new Client({
+    user: '1ung',
+    host: '127.0.0.1',
+    database: 'pokemons',
+    port: 5432
+  });
+
+  let pokeData = req.body;
+
+  let query = "DELETE FROM pokemon WHERE id = $1;";
+  let values = [params.id];
+
+  client.connect((err) => {
+    if (err) {
+      console.log("connection error", err.message);
+    }
+    client.query(query, values, (err, res) => {
+      if (err) {
+        console.log("query error", err.message);
+      }
+      response.redirect('/');
+      client.end();
+    });
+  });
+});
+
+app.get('/:id', (request, response) => {
+
+  let client = new Client({
+    user: '1ung',
+    host: '127.0.0.1',
+    database: 'pokemons',
+    port: 5432
+  });
+
+  let context = {};
+
+  client.connect((err) => {
+    if (err) {
+      console.log("connection error", err.message);
+    }
+
+    let query = "SELECT * FROM pokemon WHERE id = $1;";
+    values = [request.params.id];
+
+    client.query(query, values, (err, res) => {
+
+      if (err) {
+        console.log("query error", err.message);
+      } else {
+        context.pokemon = res.rows[0];
+      }
+      response.render('pokemon', context);
+      client.end();
+    });
+  });
+});
+
+app.get('/', (request, response) => {
   // query database for all pokemon
+  let client = new Client({
+    user: '1ung',
+    host: '127.0.0.1',
+    database: 'pokemons',
+    port: 5432
+  });
 
+  let context = {
+    "pokemon": []
+  };
+  client.connect((err) => {
+    if (err) {
+      console.log("connection error", err.message);
+    }
+
+    let query = "SELECT * FROM pokemon;"
+
+    client.query(query, (err, res) => {
+      if (err) {
+        console.log("query error", err.message);
+      } else {
+        res.rows.forEach((pokemon) => {
+          context.pokemon.push(pokemon);
+        });
+      }
+      response.render('home', context);
+      client.end();
+    });
+  });
   // respond with HTML page displaying all pokemon
+});
+
+app.post('/', (request, response) => {
+  let client = new Client({
+    user: '1ung',
+    host: '127.0.0.1',
+    database: 'pokemons',
+    port: 5432
+  });
+  let newPokemon = request.body;
+
+  let query = "INSERT INTO pokemon(name, img, weight, height) VALUES($1, $2, $3, $4);"
+  let values = [newPokemon.name, newPokemon.img, newPokemon.weight, newPokemon.height];
+
+  client.connect((err) => {
+    client.query(query, values, (err, res) => {
+      if (err) {
+        console.log("query error", err.message);
+      }
+      response.redirect('/');
+      client.end();
+    });
+  });
 });
 
 app.get('/new', (request, response) => {
@@ -49,6 +220,14 @@ app.get('/new', (request, response) => {
 
 
 app.post('/pokemon', (req, response) => {
+
+  let client = new Client({
+    user: '1ung',
+    host: '127.0.0.1',
+    database: 'pokemons',
+    port: 5432
+  });
+
   let params = req.body;
 
   const queryString = 'INSERT INTO pokemon(name, height) VALUES($1, $2)'
